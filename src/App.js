d3ccdb2aad12092bc951d298bcbe19e2e05a26cb
timotheemcdonald <timotheemcdonald@gmail.com>
2020-11-07 12:31:10 -0500

    added blue/pink color toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import {Switch, Link, Route} from 'react-router-dom'
 import styled from 'styled-components'
 import './index.css';
@@ -8,20 +8,38 @@ import Contact from './components/Contact'
 import About from './components/About'
 import Home from './components/Home'
 
+// const StyledHeader = styled.header`
+
+// `
+
 function App() {
+  const [pink, setPink] = useState(false)
+
+  function makeitpink(e){
+    e.preventDefault();
+    setPink(!pink) 
+  }
+
+
   return (
-    <div className="App">
-      <header>
-
-<div><h1>Fairy Tale Maker</h1></div>
-
-        <nav>
+    <div className={pink ? "App pinkback" : "App blueback"}>
+      <header className={pink ? "pink" : "blue"}>
+      <div><h1>Fairy Tale Maker</h1></div>
+      <nav>
         <Link to="/">Home</Link>
         <Link to="/main">Story Maker</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
         </nav>
       </header>
+ 
+      <div class="toggle">
+      <button className={pink ? "makeitpink pinkback" : "makeitpink blueback"} onClick={makeitpink}>
+      <span className={pink ? "display" : "none" }>&#129498;</span>
+        {pink ? `Make it Blue` : "Make it Pink"}
+        <span className={pink ? "none" : "display"}>&#129498;</span>
+        </button>
+      </div>
 
       <div>
         <Switch>
@@ -39,6 +57,16 @@ function App() {
           </Route>
         </Switch>
       </div>
+
+      <footer className={pink ? "pink" : "blue"}>
+      <div><h1>Fairy Tale Maker</h1></div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/main">Story Maker</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        </nav>
+      </footer>
     </div>
   );
 }
